4ad952fb859ab3d4d1e4898490fb888f8f92f6d4
K.C.KEITH CHIANG <chiangkaichih@gmail.com>
2018-12-07 17:12:13 +0800

    identify why user get 403 and give a proper response (#44)

--- server.js
+++ server.js
@@ -123,8 +123,11 @@ function render (req, res, next) {
     } else if(err.code === 404) {
       return res.status(404).send('404 | Page Not Found')
     } else if (err.code === 403) {
-      // return res.status(403).send('Please login through readr-site or you dont have any permission at this moment.')
-      return res.redirect(302, '/login')
+      if (cookies.get('csrf')) {
+        return res.status(403).send('Please login through readr-site or you dont have any permission at this moment.')
+      } else {
+        return res.redirect(302, '/login')
+      }
     } else {
       // Render Error Page or Redirect
       console.error(`error during render : ${req.url}`)
