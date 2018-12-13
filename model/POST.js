b5fc97e8bda225553f79604377c6abb991530e30
Kai-Chih Keith Chiang <chiangkaichih@gmail.com>
2018-12-13 13:02:46 +0800

    have projectId be after type for post model when editing post

--- model/POST.js
+++ model/POST.js
@@ -58,11 +58,11 @@ export const model = [
   { name: 'updatedBy', type: 'TextInput', group: 'info', width: { list: '180', editor: '200' }, isEditable: false, isListable: false, isHidden: true, },
 
   { name: 'type', type: 'Dropdownlist', group: 'basic', width: { list: '50', editor: '400' }, isEditable: true, isListable: true, options: type_options, fetchSource: typeOpts, required: true, },
+  { name: 'projectId', type: 'Dropdownlist', group: 'basic', width: { list: '110', editor: '400' }, isEditable: true, isListable: true, isNumSentitive: true, fetchSource, default: '0', defaultText: 'NA' },
   { name: 'authors', type: 'TextTagItem', group: 'basic', width: { list: '80', editor: '400' }, isEditable: true, isListable: false, map: { name: 'nickname', value: 'id',  }, autocomplete: authorAutoComplete, },
   { name: 'postOrder', type: 'TextInput', group: 'basic', width: { list: '50', editor: '80' }, isEditable: true, isListable: false, isNumSentitive: true, },
 
   { name: 'title', type: 'TextInput', group: 'content', width: { list: '400', editor: '500' }, isEditable: true, isListable: true, isEditEntry: true, },
-  { name: 'projectId', type: 'Dropdownlist', group: 'content', width: { list: '110', editor: '400' }, isEditable: true, isListable: true, isNumSentitive: true, fetchSource, default: '0', defaultText: 'NA' },
   { name: 'content', type: 'ContentEditor', group: 'content', width: { list: '180', editor: '200' }, isEditable: true, isListable: false, },
 
   // For post.type === review
