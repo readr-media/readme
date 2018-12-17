3d22f7e5d48fd138965493989bc98a3bc5631efc
K.C.KEITH CHIANG <chiangkaichih@gmail.com>
2018-12-17 15:13:57 +0800

    fix uploading img issue for editor (#62)
    * take FilePond as the uploader lib
    
    * fix uploading img issue for editor

--- src/components/item/Item.vue
+++ src/components/item/Item.vue
@@ -44,7 +44,7 @@
       theme="editor"
       :text="$t(`${$store.getters.modelName}.${itemObj.subText}`)"
       :value.sync="value"></CheckboxItem>
-    <!--Uploader v-else-if="obj.type === 'Uploader'"></Uploader-->
+    <Uploader v-else-if="itemObj.type === 'Uploader'"></Uploader>
   </div>
   <div v-else>
     <span v-if="itemObj.type === 'RadioItem'" v-text="mapValue(itemObj.name, itemObj.options, itemVal)"></span>
