14a86b381f3b9cdc98496df97930008cb08be31e
K.C.KEITH CHIANG <chiangkaichih@gmail.com>
2019-02-11 18:08:40 +0800

    fix embeded code issue (#137)

--- src/components/form/Quill/custom.js
+++ src/components/form/Quill/custom.js
@@ -51,16 +51,19 @@ export const registerHr = () => {
 export const registerEmbed = () => {
   return new Promise(resolve => {
     const BlockEmbed = Quill.import('blots/block/embed')
-    class Embbed extends BlockEmbed {
+    class ReadmeEmbbed extends BlockEmbed {
       static create(value) {
         const node = super.create(value)
         node.innerHTML = value
         return node
       }
+      static value(node) {
+        return node.innerText
+      }
     }
-    Embbed.blotName = 'embed'
-    Embbed.tagName = 'div'
-    Quill.register({ 'formats/embed': Embbed, })
+    ReadmeEmbbed.blotName = 'embed'
+    ReadmeEmbbed.tagName = 'div'
+    Quill.register({ 'formats/embed': ReadmeEmbbed, })
     resolve()
   })
 }
