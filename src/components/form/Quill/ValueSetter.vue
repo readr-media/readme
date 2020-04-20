fc3327567b207deea05f80019fe1a6836fd83936
yeefun <szyeefun@gmail.com>
2020-04-20 15:26:48 +0800

    feat: iframe google map

--- src/components/form/Quill/ValueSetter.vue
+++ src/components/form/Quill/ValueSetter.vue
@@ -49,7 +49,7 @@
       return {
         isProcessing: false,
         val: '',
-        whitelist: [ 'www.youtube.com', 'dev.readr.tw', 'www.readr.tw', 'cloud.highcharts.com', 'public.flourish.studio', 'quiz.tryinteract.com', 'plotdb.io', 'e.infogram.com' ]
+        whitelist: [ 'www.youtube.com', 'dev.readr.tw', 'www.readr.tw', 'cloud.highcharts.com', 'public.flourish.studio', 'quiz.tryinteract.com', 'plotdb.io', 'e.infogram.com', 'www.google.com' ]
       }
     },
     methods: {
